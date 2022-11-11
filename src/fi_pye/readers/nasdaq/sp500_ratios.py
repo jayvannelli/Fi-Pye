641139cbfa461c20cc913cee8d7dc07691c80055
jayvannelli <jaymvannelli@gmail.com>
2022-11-11 11:55:34 -0500

    Finish SP500Ratios reader methods (still need docs for them).

--- src/fi_pye/readers/nasdaq/sp500_ratios.py
+++ src/fi_pye/readers/nasdaq/sp500_ratios.py
@@ -86,6 +86,32 @@ class SP500Ratios(NasdaqReader):
             },
         )
 
+    def inflation_adjusted(self, timeframe: str, limit: int = 25):
+        """Query Nasdaq / MULTPL/SP500_INFLADJ / API.
+
+        Return SP5000 12-month real dividend per share (inflation adjusted).
+
+        Parameters
+        ----------
+        timeframe :
+            Either 'month' or 'year'.
+        limit : default = 25
+            Number of rows to return
+
+        Return
+        -------
+        object : pandas.DataFrame
+            pandas.Dataframe
+        """
+        return self.data(
+            base="datasets",
+            path=f"MULTPL/SP500_INFLADJ_{_validate_timeframe(timeframe).upper()}",
+            params={
+                "rows": _validate_limit(limit),
+                "api_key": self.apikey
+            },
+        )
+
     def dividend_yield(self, timeframe: str, limit: int = 25):
         """Query Nasdaq / MULTPL/SP500_DIV_YIELD / API.
 
@@ -204,6 +230,222 @@ class SP500Ratios(NasdaqReader):
         return self.data(
             base="datasets",
             path=f"MULTPL/SP500_BVPS_{_validate_timeframe(timeframe, True).upper()}",
+            params={
+                "rows": _validate_limit(limit),
+                "api_key": self.apikey
+            },
+        )
+
+    def price_to_book_value(self, timeframe: str, limit: int = 25):
+        """Query Nasdaq / MULTPL/SP500_PBV_RATIO / API.
+
+        Parameters
+        ----------
+        timeframe :
+            Either 'quarter' or 'year'.
+        limit : default = 25
+            Number of rows to return
+
+        Return
+        -------
+        object : pandas.DataFrame
+            pandas.Dataframe
+        """
+        return self.data(
+            base="datasets",
+            path=f"MULTPL/SP500_PBV_RATIO_{_validate_timeframe(timeframe, True).upper()}",
+            params={
+                "rows": _validate_limit(limit),
+                "api_key": self.apikey
+            },
+        )
+
+    def price_to_earnings(self, timeframe: str, limit: int = 25):
+        """Query Nasdaq / MULTPL/SP500_PE_RATIO / API.
+
+        Parameters
+        ----------
+        timeframe :
+            Either 'month' or 'year'.
+        limit : default = 25
+            Number of rows to return
+
+        Return
+        -------
+        object : pandas.DataFrame
+            pandas.Dataframe
+        """
+        return self.data(
+            base="datasets",
+            path=f"MULTPL/SP500_PE_RATIO_{_validate_timeframe(timeframe).upper()}",
+            params={
+                "rows": _validate_limit(limit),
+                "api_key": self.apikey
+            },
+        )
+
+    def price_to_sales(self, timeframe: str, limit: int = 25):
+        """Query Nasdaq / MULTPL/SP500_PSR_RATIO / API.
+
+        Parameters
+        ----------
+        timeframe :
+            Either 'month' or 'year'.
+        limit : default = 25
+            Number of rows to return
+
+        Return
+        -------
+        object : pandas.DataFrame
+            pandas.Dataframe
+        """
+        return self.data(
+            base="datasets",
+            path=f"MULTPL/SP500_PSR_{_validate_timeframe(timeframe, True).upper()}",
+            params={
+                "rows": _validate_limit(limit),
+                "api_key": self.apikey
+            },
+        )
+
+    def real_earnings_growth(self, timeframe: str, limit: int = 25):
+        """Query Nasdaq / MULTPL/SP500_REAL_EARNINGS_GROWTH / API.
+
+        Parameters
+        ----------
+        timeframe :
+            Either 'month' or 'year'.
+        limit : default = 25
+            Number of rows to return
+
+        Return
+        -------
+        object : pandas.DataFrame
+            pandas.Dataframe
+        """
+        return self.data(
+            base="datasets",
+            path=f"MULTPL/SP500_REAL_EARNINGS_GROWTH_{_validate_timeframe(timeframe, True).upper()}",
+            params={
+                "rows": _validate_limit(limit),
+                "api_key": self.apikey
+            },
+        )
+
+    def sales(self, timeframe: str, limit: int = 25):
+        """Query Nasdaq / MULTPL/SP500_SALES / API.
+
+        Parameters
+        ----------
+        timeframe :
+            Either 'month' or 'year'.
+        limit : default = 25
+            Number of rows to return
+
+        Return
+        -------
+        object : pandas.DataFrame
+            pandas.Dataframe
+        """
+        return self.data(
+            base="datasets",
+            path=f"MULTPL/SP500_SALES_{_validate_timeframe(timeframe, True).upper()}",
+            params={
+                "rows": _validate_limit(limit),
+                "api_key": self.apikey
+            },
+        )
+
+    def real_sales(self, timeframe: str, limit: int = 25):
+        """Query Nasdaq / MULTPL/SP500_REAL_SALES / API.
+
+        Parameters
+        ----------
+        timeframe :
+            Either 'month' or 'year'.
+        limit : default = 25
+            Number of rows to return
+
+        Return
+        -------
+        object : pandas.DataFrame
+            pandas.Dataframe
+        """
+        return self.data(
+            base="datasets",
+            path=f"MULTPL/SP500_REAL_SALES_{_validate_timeframe(timeframe, True).upper()}",
+            params={
+                "rows": _validate_limit(limit),
+                "api_key": self.apikey
+            },
+        )
+
+    def sales_growth(self, timeframe: str, limit: int = 25):
+        """Query Nasdaq / MULTPL/SP500_SALES_GROWTH / API.
+
+        Parameters
+        ----------
+        timeframe :
+            Either 'month' or 'year'.
+        limit : default = 25
+            Number of rows to return
+
+        Return
+        -------
+        object : pandas.DataFrame
+            pandas.Dataframe
+        """
+        return self.data(
+            base="datasets",
+            path=f"MULTPL/SP500_SALES_GROWTH_{_validate_timeframe(timeframe, True).upper()}",
+            params={
+                "rows": _validate_limit(limit),
+                "api_key": self.apikey
+            },
+        )
+
+    def real_sales_growth(self, timeframe: str, limit: int = 25):
+        """Query Nasdaq / MULTPL/SP500_REAL_SALES_GROWTH / API.
+
+        Parameters
+        ----------
+        timeframe :
+            Either 'month' or 'year'.
+        limit : default = 25
+            Number of rows to return
+
+        Return
+        -------
+        object : pandas.DataFrame
+            pandas.Dataframe
+        """
+        return self.data(
+            base="datasets",
+            path=f"MULTPL/SP500_REAL_SALES_GROWTH_{_validate_timeframe(timeframe, True).upper()}",
+            params={
+                "rows": _validate_limit(limit),
+                "api_key": self.apikey
+            },
+        )
+
+    def real_price(self, timeframe: str, limit: int = 25):
+        """Query Nasdaq / MULTPL/SP500_REAL_PRICE / API.
+
+        Parameters
+        ----------
+        timeframe :
+            Either 'month' or 'year'.
+        limit : default = 25
+            Number of rows to return
+
+        Return
+        -------
+        object : pandas.DataFrame
+            pandas.Dataframe
+        """
+        return self.data(
+            base="datasets",
+            path=f"MULTPL/SP500_REAL_PRICE_{_validate_timeframe(timeframe).upper()}",
             params={
                 "rows": _validate_limit(limit),
                 "api_key": self.apikey
